5a766e9c8759dd0570d2efb7f6e105abac005dc2
Manasseh Mmadu <mmadumanasseh@gmail.com>
2018-10-11 00:03:59 +0100

    Adding more descriptive comments

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 /**
  * Easy way to define Custom Exceptions By just creating New Instance
  * of Exception
- * @param name: optional The name of the exception
+ * @param name optional. The name of the exception
  * With the name parameter, exception is instantiated as
  * ExceptionName: Exception message
  * Without name parameter, exception is instantiated as
  * Error: Exception message
+ * @returns Error
  * 
  */
 
 function Exception(name) {
+    /** 
+     * Create CustomError Logic
+     * @param message optional. message to be displayed by exception
+     */
     function CustomError(message) {
-        var instance = new Error(message);
+        const instance = new Error(message);
         instance.name = name
 
+        // is function. look on to Making it prototypal
         instance.is = function(err) {
             return err === instance.name || CustomError === err
         }
